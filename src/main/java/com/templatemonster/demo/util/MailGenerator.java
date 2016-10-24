942c33afd3b2f3c8cf6e6a41edd27a6be7243a43
i.gonchar <IgorGoncharUA@gmail.com>
2016-10-24 17:49:24 +0300

    Mail generator updated

--- src/main/java/com/templatemonster/demo/util/MailGenerator.java
+++ src/main/java/com/templatemonster/demo/util/MailGenerator.java
@@ -1,5 +1,7 @@
 package com.templatemonster.demo.util;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.Random;
 
 /**
@@ -8,28 +10,10 @@ import java.util.Random;
 public class MailGenerator {
 
     public static String generateNewEmail() {
-        return generateRandomText(15) + "@com.com";
+        return generateAlphaNumericString(15) + "@com.com";
     }
 
-    private static String generateRandomText(int textLength) {
-        String characters;
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sbResult = new StringBuilder();
-        Random random = new Random();
-
-        for (char ch = 'a'; ch < 'z'; ch++) {
-            sb.append(ch);
-        }
-        for (char ch = '1'; ch < '9'; ch++) {
-            sb.append(ch);
-        }
-        characters = sb.toString();
-        char[] charsArray = new char[textLength];
-
-        for (int i = 0; i < textLength; i++) {
-            charsArray[i] = characters.charAt(random.nextInt(characters.length()));
-        }
-        String result = new String(charsArray);
-        return result;
+    private static String generateAlphaNumericString(int textLength){
+        return RandomStringUtils.randomAlphanumeric(textLength);
     }
 }
