92bc79373d5edd76de23f40c4b91870d860941e6
i.gonchar <IgorGoncharUA@gmail.com>
2016-12-06 15:48:47 +0200

    ExcelUtils.java was refactored

--- src/main/java/com/templatemonster/demo/util/ExcelUtils.java
+++ src/main/java/com/templatemonster/demo/util/ExcelUtils.java
@@ -3,53 +3,68 @@ package com.templatemonster.demo.util;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 //.xls files are managed
 
-public class ExcelUtils {
+public class ExcelUtils extends BaseUtils {
     private HSSFWorkbook excelWBook;
     private HSSFSheet excelWSheet;
     private Cell cell;
 
-    public Object[][] getTableArray(String filePath, String sheetName) throws Exception {
-        String[][] tabArray = null;
-        try {
-            FileInputStream excelFile = new FileInputStream(filePath);
-            // Access the required test data sheet
-            excelWBook = new HSSFWorkbook(excelFile);
-            excelWSheet = excelWBook.getSheet(sheetName);
-            int startRow = 1;
-            int startCol = 1;
-            int ci, cj;
-            int totalRows = excelWSheet.getLastRowNum();
-            //int totalCols = excelWSheet.getRow(1).getLastCellNum();
-            int totalCols = 3;
+    public ExcelUtils(String filePath) {
+        initializeExcelFile(filePath);
+    }
 
-            tabArray = new String[totalRows][totalCols];
-            ci = 0;
-            for (int i = startRow; i <= totalRows; i++, ci++) {
-                cj = 0;
-                for (int j = startCol; j <= totalCols; j++, cj++) {
-                    tabArray[ci][cj] = getCellData(i, j);
-                    System.out.println(tabArray[ci][cj]);
-                }
+    public Object[][] getWholeTableArrayFromExcelFile() {
+        String[][] tabArray;
+
+        int startRow = 1;
+        int startCol = 1;
+        int ci, cj;
+        int totalRows = excelWSheet.getLastRowNum();
+        int totalCols = 2;
+
+        tabArray = new String[totalRows][totalCols];
+        ci = 0;
+        for (int i = startRow; i <= totalRows; i++, ci++) {
+            cj = 0;
+            for (int j = startCol; j <= totalCols; j++, cj++) {
+                tabArray[ci][cj] = getCellData(i, j);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("Could not read the Excel sheet");
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("Could not read the Excel sheet");
-            e.printStackTrace();
         }
+
         return (tabArray);
     }
 
-    public String getCellData(int RowNum, int ColNum) throws Exception {
+    public List<String> getDataFromOneRowInExcelFile(int rowNumber) {
+        List<String> result = new ArrayList<>();
+        int noOfColumns = excelWSheet.getRow(rowNumber).getPhysicalNumberOfCells();
+
+        for (int i = 1; i < noOfColumns; i++) {
+            result.add(getCellData(rowNumber, i));
+        }
+
+        return result;
+    }
+
+    private void initializeExcelFile(String filePath) {
+        try {
+            FileInputStream excelFile = new FileInputStream(filePath);
+            excelWBook = new HSSFWorkbook(excelFile);
+            excelWSheet = excelWBook.getSheetAt(0);
+            //excelWSheet = excelWBook.getSheet(sheetName);
+        } catch (Exception e) {
+            LOGGER.error("Could not find the Excel sheet");
+            e.printStackTrace();
+        }
+    }
+
+    private String getCellData(int RowNum, int ColNum) {
         try {
             cell = excelWSheet.getRow(RowNum).getCell(ColNum);
             int dataType = cell.getCellType();
@@ -60,7 +75,7 @@ public class ExcelUtils {
                 return CellData;
             }
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            LOGGER.error(e.getMessage());
             throw (e);
         }
     }
