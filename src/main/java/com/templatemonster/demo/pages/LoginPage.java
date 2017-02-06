b65a6e670934f183e743d5d0e6483ff051a02b68
i.gonchar <IgorGoncharUA@gmail.com>
2017-02-06 12:29:45 +0200

    login test was updated

--- src/main/java/com/templatemonster/demo/pages/LoginPage.java
+++ src/main/java/com/templatemonster/demo/pages/LoginPage.java
@@ -4,6 +4,7 @@ import static com.codeborne.selenide.Condition.*;
 import com.templatemonster.demo.pages.basePages.BasePage;
 import com.templatemonster.demo.pages.pagesWithHeader.HomePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -15,9 +16,9 @@ import static com.codeborne.selenide.WebDriverRunner.url;
  * Created by i.gonchar on 29.09.2016.
  */
 public class LoginPage extends BasePage {
-    private By LOGIN_FORM_SUBMIT_BUTTON_LOCATOR = By.id("login_button");
-    private By USER_LOGIN_INPUT_LOCATOR = By.id("edtLogin");
-    private By USER_PASSWORD_INPUT_LOCATOR = By.id("edtPassword");
+    private By LOGIN_FORM_SUBMIT_BUTTON_LOCATOR = By.xpath("//button[contains(.,'Log in')]");
+    private By USER_LOGIN_INPUT_LOCATOR = By.xpath("//input[@type='email']");
+    private By USER_PASSWORD_INPUT_LOCATOR =By.xpath("//input[@type='password']");
     private By LOGIN_ERROR_LABEL_LOCATOR = By.id("edtPassword");
     private By USER_REGISTER_LINK_TEXT = By.linkText("register here for free");
 
@@ -27,6 +28,7 @@ public class LoginPage extends BasePage {
 
     public LoginPage fillLoginFormWithAnyCredentials(String login, String password) {
         $(USER_LOGIN_INPUT_LOCATOR).shouldBe(visible).sendKeys(login);
+        $(USER_LOGIN_INPUT_LOCATOR).sendKeys(Keys.RETURN);
         $(USER_PASSWORD_INPUT_LOCATOR).shouldBe(visible).sendKeys(password);
         return this;
     }
