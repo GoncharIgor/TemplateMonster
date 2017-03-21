2db923227ee5cf134a2e8d0ce42012cab4c74c2d
igorg <igorg@backbase.com>
2017-03-21 12:01:47 +0100

    getDifferenceOfImagesInPercent() method was added

--- src/main/java/com/templatemonster/demo/util/ScreenShotUtil.java
+++ src/main/java/com/templatemonster/demo/util/ScreenShotUtil.java
@@ -9,6 +9,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -36,12 +37,52 @@ public class ScreenShotUtil {
         int eleHeight = ele.getSize().getHeight();
 
         // Crop the entire page screenshot to get only element screenshot
-        BufferedImage eleScreenshot= fullImg.getSubimage(point.getX(), point.getY(),
+        BufferedImage eleScreenshot = fullImg.getSubimage(point.getX(), point.getY(),
                 eleWidth, eleHeight);
         ImageIO.write(eleScreenshot, "png", screenshot);
 
         // Copy the element screenshot to disk
-        FileUtils.copyFile(screenshot, new File("screenSchots/" + screenShotName + ".png"));
+        FileUtils.copyFile(screenshot, new File("screenShots/" + screenShotName + ".png"));
+    }
+
+    public BufferedImage getImageFromFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        return ImageIO.read(file);
+    }
+
+    public double getDifferenceOfImagesInPercent(BufferedImage img1, BufferedImage img2) {
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        int width1 = img1.getWidth(null);
+        int width2 = img2.getWidth(null);
+        int height1 = img1.getHeight(null);
+        int height2 = img2.getHeight(null);
+
+        if ((width1 != width2) || (height1 != height2)) {
+            System.err.println("Error: Images dimensions mismatch");
+        }
+
+        long diff = 0;
+        for (int y = 0; y < height1; y++) {
+            for (int x = 0; x < width1; x++) {
+                int rgb1 = img1.getRGB(x, y);
+                int rgb2 = img2.getRGB(x, y);
+                int r1 = (rgb1 >> 16) & 0xff;
+                int g1 = (rgb1 >> 8) & 0xff;
+                int b1 = (rgb1) & 0xff;
+                int r2 = (rgb2 >> 16) & 0xff;
+                int g2 = (rgb2 >> 8) & 0xff;
+                int b2 = (rgb2) & 0xff;
+                diff += Math.abs(r1 - r2);
+                diff += Math.abs(g1 - g2);
+                diff += Math.abs(b1 - b2);
+            }
+        }
+        double n = width1 * height1 * 3;
+        double p = diff / n / 255.0;
+        // System.out.println("diff percent: " + (p * 100.0));
+
+        return Double.parseDouble(df.format(p * 100.0));
     }
 
 }
