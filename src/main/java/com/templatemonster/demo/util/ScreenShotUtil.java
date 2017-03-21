5d280d0396cab0c78ba12b15d367344055c3c7db
igorg <igorg@backbase.com>
2017-03-21 10:46:45 +0100

    Screenshot utils were updated

--- src/main/java/com/templatemonster/demo/util/ScreenShotUtil.java
+++ src/main/java/com/templatemonster/demo/util/ScreenShotUtil.java
@@ -1,12 +1,16 @@
 package com.templatemonster.demo.util;
 
+import com.codeborne.selenide.SelenideElement;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class ScreenShotUtil {
     private WebDriver driver;
@@ -17,7 +21,27 @@ public class ScreenShotUtil {
 
     public void takeScreenShot(String screenShotName) throws IOException {
         File screenShotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenShotFile, new File("screenSchots/" + screenShotName + ".png"));
+        FileUtils.copyFile(screenShotFile, new File("screenShots/" + screenShotName + ".png"));
+    }
+
+    public void takeScreenShotFromPartOfThePage(String screenShotName, By locator) throws IOException {
+        SelenideElement ele = $(locator);
+        // Get entire page screenshot
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        BufferedImage fullImg = ImageIO.read(screenshot);
+
+        //  Get the location of element on the page and its width and height
+        Point point = ele.getLocation();
+        int eleWidth = ele.getSize().getWidth();
+        int eleHeight = ele.getSize().getHeight();
+
+        // Crop the entire page screenshot to get only element screenshot
+        BufferedImage eleScreenshot= fullImg.getSubimage(point.getX(), point.getY(),
+                eleWidth, eleHeight);
+        ImageIO.write(eleScreenshot, "png", screenshot);
+
+        // Copy the element screenshot to disk
+        FileUtils.copyFile(screenshot, new File("screenSchots/" + screenShotName + ".png"));
     }
 
 }
