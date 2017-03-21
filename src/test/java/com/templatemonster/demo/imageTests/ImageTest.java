b794489f411661c70e2f469fa7c3dd40e5bb3cd3
igorg <igorg@backbase.com>
2017-03-21 12:01:24 +0100

    compare2Images() test was added

---
+++ src/test/java/com/templatemonster/demo/imageTests/ImageTest.java
@@ -0,0 +1,35 @@
+package com.templatemonster.demo.imageTests;
+
+import com.templatemonster.demo.baseTests.TemplateMonsterBaseTest;
+import com.templatemonster.demo.util.ScreenShotUtil;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by i.gonchar on 21/03/2017.
+ */
+public class ImageTest extends TemplateMonsterBaseTest {
+    private ScreenShotUtil screenShotUtil;
+    private By logoLocator = By.cssSelector(".logo-icon.svg");
+
+    @Test
+    public void checkLogoOnHomePage() throws IOException {
+        screenShotUtil = new ScreenShotUtil(driver);
+
+        openHomePage();
+        screenShotUtil.takeScreenShotFromPartOfThePage("aaa", logoLocator);
+    }
+
+    @Test
+    public void compare2Images() throws IOException {
+        screenShotUtil = new ScreenShotUtil(driver);
+        BufferedImage img1 = screenShotUtil.getImageFromFile("screenShots/aaa.png");
+        BufferedImage img2 = screenShotUtil.getImageFromFile("screenShots/bbb.png");
+
+        double a = screenShotUtil.getDifferenceOfImagesInPercent(img1, img2);
+        System.out.println("Difference is: " + a);
+    }
+}
